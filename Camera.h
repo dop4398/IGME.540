6fcf62023c4b5b3fdd4a02b1a6cdbe87eabb6a62
dop4398 <dop4398@IGMOH1375-10>
2020-02-06 10:40:50 -0500

    minor edits

--- Camera.h
+++ Camera.h
@@ -1,11 +1,20 @@
 #pragma once
+#include "Game.h"
+
 class Camera
 {
 public:
 	Camera(float x, float y, float z, float aspectRatio, float mouseLookSpeed);
 	~Camera();
 
+	// Updating
+	void Update();
+	void UpdateViewMatrix();
+	void UpdateProjectionMatrix();
+
 private:
-	
+	// Matrices
+	DirectX::XMFLOAT4X4 viewMatrix;
+	DirectX::XMFLOAT4X4 projectionMatrix;
 };
 
